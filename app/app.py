a70dbf69a7e7b4824629c62760628fc234fec62c
Coderklng <kkartiksharma999@gmail.com>
2026-07-02 21:39:44 +0530

    changes detected app.py file

--- app/app.py
+++ app/app.py
@@ -54,4 +54,4 @@ def get_session_history(session_id):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.app:app", host="0.0.0.0", port=8000)
